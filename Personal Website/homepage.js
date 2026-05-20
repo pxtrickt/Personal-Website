4588395f59d59e19ab7a1b2dfaa9033ef24d2238
Patrick Thomas <p4trickthom4as@gmail.com>
2026-05-20 18:30:06 +0100

    Add files via upload

---
+++ Personal Website/homepage.js
@@ -0,0 +1,13 @@
+const text = "Hey, I'm Patrick.";
+const speed = 180; 
+let index = 0;
+
+function typeEffect() {
+  if (index < text.length) {
+    document.getElementById("typing").textContent += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, speed);
+  }
+}
+
+typeEffect();
